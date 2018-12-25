79f59e90c323a1c40b68ee2973abf33af468d9e6
Yi Congwei <43376365+HafniumYi@users.noreply.github.com>
2018-12-25 16:46:29 +0800

    Update wcount.py

--- pyassign4/wcount.py
+++ pyassign4/wcount.py
@@ -8,19 +8,21 @@ __email__  = "yicw@pku.edu.cn"
 import sys
 import collections 
 from urllib.request import urlopen
+import urllib.error
 
 def wcount(lines, topn=10):
     """count words from lines of text string, then sort by their counts
     in reverse order, output the topn (word count), each in one line. 
     """
-    lines=lines.lower()
+    lines = lines.lower()
     for i in lines:
-        if not i.isalpha():#处理非单词部分（包括将I'm一类抽得到I）
+        if not i.isalpha():#判断到非单词部分改成空格
             lines=lines.replace(i,' ')
-    vocabulary=lines.split()
-    ct = collections.Counter(vocabulary)
-    for (a,b) in ct.most_common(topn):
+    vocabulary = lines.split()
+    ct = collections.Counter(vocabulary)#利用counter得到计数dict
+    for (a,b) in ct.most_common(topn):#利用most_common得到按key的排序
         print(a,'\t',b)
+    
     pass
 
 if __name__ == '__main__':
@@ -32,27 +34,25 @@ if __name__ == '__main__':
         sys.exit(1)
     #anayze whether paras are right or not
     elif len(sys.argv) >= 2:
-        try:#将txt网页的url读作字符串。
-            book = urlopen(sys.argv[1])
+        try:
+            book = urlopen(sys.argv[1])#利用urlopen得到url
             ibook = book.read()
             book.close()
-        except ValueError:# 各类报错
-            print('unknown url type. please check your website,we got invaild url from it.')
-        except urllib.error.URLError:
-            print('Errno 11001. Please check your website or your Internet connection')
-        except urllib.error.HTTPError:
-            print('HTTP Error 404: Not Found. Please check your website.')
+        except ValueError:#各种报错类型
+            print('ValueError:unknown url type. please check your website,we got invaild url from it.')
+        except urllib.error.URLError :
+            print('urllib.error.URLError:Errno 11001. Please check your website or your Internet connection')
+        except urllib.error.HTTPError :
+            print('urllib.error.URLError:HTTP Error 404: Not Found. Please check your website.')
         else:
-            bookstr=ibook.decode()
-            if len(sys.argv) >= 3:
+            bookstr = ibook.decode()
+            if len(sys.argv) >= 3:#寻找topn值
                 try:
-                    topn=int(sys.argv[2])
+                    topn = int(sys.argv[2])
                 except ValueError:
                     print('please insure the number that you input is vaild.')
                 else:
-                    topn=int(sys.argv[2])
+                    topn = int(sys.argv[2])
                     wcount(bookstr,topn)
-                
             else:
                 wcount(bookstr)
-            
